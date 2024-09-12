83c31345e83123e337376a1c3c30ef01fbaa8921
biancavida <biancavida609@gmail.com>
2024-09-12 20:52:05 -0300

    Add files via upload

---
+++ Ex03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        int A,B, C;
+        System.out.println("Insira o primeiro numero: ");
+        A = input.nextInt();
+        System.out.println("Insira o segundo numero: ");
+        B = input.nextInt();
+        System.out.println("Insira o terceiro numero: ");
+        C = input.nextInt();
+        
+        input.close();
+    }
+}
+
+        
